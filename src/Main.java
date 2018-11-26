ae9390e60e122c728f868bfed9b76f93c6a10521
Keita TAMANAHA <e185755@ie.u-ryukyu.ac.jp>
2018-11-27 02:46:57 +0900

    step1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         String str = null;
-        System.out.println(str.length());
+
+        try{
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("エラー「NullPointerException」が発生");
+            System.out.println(e.getMessage());
+        }
+
+
+
     }
 }
+
